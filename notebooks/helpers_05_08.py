9f9dd5dab076066b3ec98f7c17e3b627d71233c3
Martin Szyska <martin.szyska37@gmail.com>
2020-10-22 08:12:25 +0200

    finished GMM

--- notebooks/helpers_05_08.py
+++ notebooks/helpers_05_08.py
@@ -3,6 +3,73 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
 from ipywidgets import interact
+from sklearn.metrics import pairwise_distances_argmin
+from sklearn.datasets import make_blobs
+
+
+def plot_kmeans_interactive(clusters=[1,3,6], frames=[0, 50]):
+    '''Interactive K-Means
+
+    The following script uses IPython's interactive widgets to demonstrate the K-means algorithm interactively.
+    Run this within the IPython notebook to explore the expectation maximization algorithm for computing K Means.
+    '''
+    X, y = make_blobs(n_samples=300, centers=4,
+                      random_state=0, cluster_std=0.60)
+        
+    def plot_points(X, labels, n_clusters):
+        plt.scatter(X[:, 0], X[:, 1], c=labels, s=50, cmap='viridis',
+                    vmin=0, vmax=n_clusters - 1);
+            
+    def plot_centers(centers):
+        plt.scatter(centers[:, 0], centers[:, 1], marker='o',
+                    c=np.arange(centers.shape[0]),
+                    s=200, cmap='viridis')
+        plt.scatter(centers[:, 0], centers[:, 1], marker='o',
+                    c='black', s=50)
+            
+
+    def _kmeans_step(frame=0, n_clusters=4):
+        rng = np.random.RandomState(2)
+        labels = np.zeros(X.shape[0])
+        i = rng.permutation(X.shape[0])[:n_clusters]
+        centers = X[i]
+
+        nsteps = frame // 3
+
+        for i in range(nsteps + 1):
+            old_centers = centers
+            if i < nsteps or frame % 3 > 0:
+                labels = pairwise_distances_argmin(X, centers)
+
+            if i < nsteps or frame % 3 > 1:
+                centers = np.array([X[labels == j].mean(0)
+                                    for j in range(n_clusters)])
+                nans = np.isnan(centers)
+                centers[nans] = old_centers[nans]
+
+        # plot the data and cluster centers
+        plot_points(X, labels, n_clusters)
+        plot_centers(old_centers)
+
+        # plot new centers if third frame
+        if frame % 3 == 2:
+            for i in range(n_clusters):
+                plt.annotate('', centers[i], old_centers[i], 
+                             arrowprops=dict(arrowstyle='->', linewidth=1))
+            plot_centers(centers)
+
+        plt.xlim(-4, 4)
+        plt.ylim(-2, 10)
+
+        if frame % 3 == 1:
+            plt.text(3.8, 9.5, "1. Reassign points to nearest centroid",
+                     ha='right', va='top', size=14)
+        elif frame % 3 == 2:
+            plt.text(3.8, 9.5, "2. Update centroids to cluster means",
+                     ha='right', va='top', size=14)
+    
+    return interact(_kmeans_step, frame=frames,
+                    n_clusters=clusters)
 
 
 def visualize_tree(estimator, X, y, boundaries=True,
